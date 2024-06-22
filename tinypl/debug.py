f58618b4ea58eed03f979973beeb3a487c685c5b
Florian Fervers <florian.fervers@gmail.com>
2024-06-22 09:48:22 +0200

    Change print_on_item to print before and after pull

--- tinypl/debug.py
+++ tinypl/debug.py
@@ -1,8 +1,19 @@
-import tinypl as pl
+from . import pipe
 
-def print_on_item(pipe, msg_fn=lambda x: str(x)):
-    if isinstance(msg_fn, str):
-        msg_fn = lambda item, result=msg_fn: result
-    def mapper(item):
-        print(msg_fn(item))
-    return pl.each(mapper, pipe)
+class print_on_item(pipe.Pipe):
+    def __init__(self, input, msg_fn):
+        super().__init__()
+        self.input = pipe.wrap(input)
+
+        if isinstance(msg_fn, str):
+            msg_fn = lambda item=None, result=msg_fn: result
+        self.msg_fn = msg_fn
+
+    def stop(self):
+        self.input.stop()
+
+    def _next(self):
+        print(f"PIPE: Pulling   {self.msg_fn()}")
+        item = pipe._next(self.input)
+        print(f"PIPE: Returning {self.msg_fn(item)}")
+        return item
